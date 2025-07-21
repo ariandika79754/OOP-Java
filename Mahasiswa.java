49285661a7fe21b89d2f555c8f0de48446531867
Ari Andika <ariandika79754@gmail.com>
2025-07-21 11:10:15 +0700

    upload by ari

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,19 +1,26 @@
 public class Mahasiswa {
-    String nama;
-    int umur;
+    // Atribut
+    private String nama;
+    private int umur;
 
+    // Constructor
     public Mahasiswa(String nama, int umur) {
         this.nama = nama;
         this.umur = umur;
     }
 
-    void tampilkanData() {
+    // Method untuk menampilkan data
+    public void tampilkanData() {
         System.out.println("Nama: " + nama);
         System.out.println("Umur: " + umur);
     }
 
+    // Method main untuk menjalankan program
     public static void main(String[] args) {
+        // Membuat objek Mahasiswa
         Mahasiswa mhs = new Mahasiswa("Budi", 21);
+
+        // Menampilkan data mahasiswa
         mhs.tampilkanData();
     }
 }
